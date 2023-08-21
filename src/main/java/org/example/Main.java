7f38cbad31bd4c356c83fbe1e7f9af055587ace4
FaridaFatali <faridafatali.91@gmail.com>
2023-08-21 14:16:53 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Practice: https://www.youtube.com/watch?v=0cATENiMsBE
+ * @author Farida Fatali
+ * Creating simple StopWatch App
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+    }
+}
